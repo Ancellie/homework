cf08fad3032c116de09a9e2c9534304606baf420
Ancellie <xnazar507@gmail.com>
2023-02-26 18:55:20 +0200

    version-1.1

---
+++ main.js
@@ -0,0 +1,19 @@
+let one = require('./testOne.js');
+let two = require('./testTwo.js');
+let three = require('./testThree.js');
+let five = require('./testFive.js');
+let six = require('./testSix.js');
+
+one.testOne()
+console.log('Test one passed successfully');
+two.test_two()
+console.log('Test two1 passed successfully')
+two.test_two1()
+console.log('Test two2 passed successfully')
+three.testThree()
+console.log('Test three passed successfully')
+five.testFive()
+console.log('Test five passed successfully')
+six.testSix()
+console.log('Test six passed successfully')
+
